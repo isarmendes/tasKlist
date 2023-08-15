c48e9cc728b585e423fb9ff2054af3ae4a157e61
isarmendes <isadoramendes@aluno.senai.br>
2023-08-15 16:37:23 -0300

    feat: finish css, html

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+class Task{
+    constructor(id,title,status){
+        this.id = id;
+        this.title = title;
+        this.status = status
+    }
+}
+
+class TaskList {
+    constructor(){
+        this.Task =[]
+    }
+}
+
